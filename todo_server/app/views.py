d23d068ce595ecb9169fca18b93e53232a87f7f8
nilaster <nilaster@riseup.net>
2024-05-09 16:31:26 +0200

    feat: add health check view

--- todo_server/app/views.py
+++ todo_server/app/views.py
@@ -1,7 +1,14 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from . import models, serializers
 
 
 class TaskViewSet(ModelViewSet):
     queryset = models.Task.objects.all()
     serializer_class = serializers.TaskSerializer
+
+
+class HealthCheckView(APIView):
+    def get(self, request):
+        return Response(status=200)
--- todo_server/urls.py
+++ todo_server/urls.py
@@ -1,9 +1,12 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
-from todo_server.app.views import TaskViewSet
+from todo_server.app.views import TaskViewSet, HealthCheckView
 
 
 router = SimpleRouter()
 router.register("tasks", TaskViewSet)
 
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("/", HealthCheckView.as_view(), name="health-check"),
+] + router.urls
