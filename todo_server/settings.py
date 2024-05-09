a481c4d909fa33a59a8860cc1bb3f2ded38df984
nilaster <nilaster@riseup.net>
2024-05-09 15:54:10 +0200

    Add health check for EB

--- todo_server/settings.py
+++ todo_server/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "ebhealthcheck.apps.EBHealthCheckConfig",
     "corsheaders",
     "todo_server.app",
 ]
