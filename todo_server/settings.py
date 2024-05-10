3036c3bae27692f6fc6d0414ddfd7704cd127cd2
nilaster <nilaster@riseup.net>
2024-05-10 11:47:59 +0200

    add dotenv variable parsing for settings

--- todo_server/settings.py
+++ todo_server/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
@@ -21,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i-^7wbkgw75w&l74s3!2dhr32va9to=sh@q93n-rytma%3m)%2"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY", "django-insecure-i-^7wbkgw75w&l74s3!2dhr32va9to=sh@q93n-rytma%3m)%2"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [
     "localhost",
@@ -147,5 +152,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://master.d2cnc16o5lf1bi.amplifyapp.com/",
+    "https://master.d2cnc16o5lf1bi.amplifyapp.com",
 ]
